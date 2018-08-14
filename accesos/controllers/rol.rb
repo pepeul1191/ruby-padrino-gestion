2079cac2011df113d76d8e717c0e5d2d0d413b2f
Pepe Valdivia <jvaldivia@softweb.pe>
2018-08-14 12:27:15 -0500

    Asociar permisos a roles

--- accesos/controllers/rol.rb
+++ accesos/controllers/rol.rb
@@ -86,4 +86,85 @@ App::Accesos.controllers :rol do
     status status
     rpta.to_json
   end
+
+  get :listar_permisos, :map => '/rol/permiso/listar/:rol_id' do
+    rpta = []
+    status = 200
+    begin
+      rol_id = params['rol_id']
+      rpta = DB_ACCESOS.fetch('
+    		SELECT T.id AS id, T.nombre AS nombre, (CASE WHEN (P.existe = 1) THEN 1 ELSE 0 END) AS existe, T.llave AS llave FROM
+    		(
+    			SELECT id, nombre, llave, 0 AS existe FROM permisos
+    		) T
+    		LEFT JOIN
+    		(
+    			SELECT P.id, P.nombre,  P.llave, 1 AS existe  FROM permisos P
+    			INNER JOIN roles_permisos RP ON P.id = RP.permiso_id
+    			WHERE RP.rol_id =  ' + rol_id + '
+    		) P
+    		ON T.id = P.id').to_a
+    rescue Exception => e
+      status = 500
+      rpta = {
+        :tipo_mensaje => 'error',
+        :mensaje => [
+          'Se ha producido un error en listar los permosos del rol',
+          e.message
+        ]}
+    end
+    status status
+    rpta.to_json
+  end
+
+  post :guardar_permisos, :map => '/rol/permiso/guardar' do
+    data = JSON.parse(params[:data])
+    editados = data['editados']
+    rol_id = data['extra']['rol_id']
+    rpta = []
+    status = 200
+    DB_ACCESOS.transaction do
+      begin
+        if editados.length != 0
+          editados.each do |editado|
+            existe = editado['existe']
+            permiso_id = editado['id']
+            e = Models::Accesos::RolPermiso.where(
+              :permiso_id => permiso_id,
+              :rol_id => rol_id
+            ).first
+            if existe == 0 #borrar si existe
+              if e != nil
+                e.delete
+              end
+            elsif existe == 1 #crear si no existe
+              if e == nil
+                n = Models::Accesos::RolPermiso.new(
+                  :permiso_id => permiso_id,
+                  :rol_id => rol_id
+                )
+                n.save
+              end
+            end
+          end
+        end
+        rpta = {
+          :tipo_mensaje => 'success',
+          :mensaje => [
+            'Se ha registrado la asociación de permisos al rol',
+          ]}
+      rescue Exception => e
+        Sequel::Rollback
+        status = 500
+        rpta = {
+          :tipo_mensaje => 'error',
+          :mensaje => [
+            'Se ha producido un error en asociar los permisos al rol',
+            e.message
+          ]}
+      end
+    end
+    status status
+    rpta.to_json
+  end
 end
---
+++ accesos/models/rol_permiso_model.rb
@@ -0,0 +1,7 @@
+module Models
+  module Accesos
+    class RolPermiso < Sequel::Model(DB_ACCESOS[:roles_permisos])
+
+    end
+  end
+end
