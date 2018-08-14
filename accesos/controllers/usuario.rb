32ee1ffdaa9d24a0f900269f249aec00a5be9935
Pepe Valdivia <jvaldivia@softweb.pe>
2018-08-14 14:00:31 -0500

    Obtener usuario, correo y estado

--- accesos/controllers/usuario.rb
+++ accesos/controllers/usuario.rb
@@ -20,4 +20,23 @@ App::Accesos.controllers :usuario do
     status status
     rpta.to_json
   end
+
+  get :usuario_correo_estado, :map => '/usuario/obtener_usuario_correo/:usuario_id' do
+    rpta = []
+    status = 200
+    begin
+      usuario_id = params['usuario_id']
+      rpta = Models::Accesos::VWUsuarioCorreoEstado.where(:id => usuario_id).first()
+    rescue Exception => e
+      status = 500
+      rpta = {
+        :tipo_mensaje => 'error',
+        :mensaje => [
+          'Se ha producido un error en obtener los datos del usuario',
+          e.message
+        ]}
+    end
+    status status
+    rpta.to_json
+  end
 end
---
+++ accesos/models/vw_usuario_correo_estado_model.rb
@@ -0,0 +1,7 @@
+module Models
+  module Accesos
+    class VWUsuarioCorreoEstado < Sequel::Model(DB_ACCESOS[:vw_usuario_correo_estado])
+
+    end
+  end
+end
