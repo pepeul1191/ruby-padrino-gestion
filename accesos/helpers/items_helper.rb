8cc675741c73f38fbd8abc0730a30f8d61425f50
Pepe Valdivia <jvaldivia@softweb.pe>
2018-08-14 10:55:11 -0500

    CRUD de items

---
+++ accesos/helpers/items_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module App
+  class Accesos
+    module ItemsHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers ItemsHelper
+  end
+end
---
+++ accesos/models/item_model.rb
@@ -0,0 +1,7 @@
+module Models
+  module Accesos
+    class Item < Sequel::Model(DB_ACCESOS[:items])
+
+    end
+  end
+end
