654f6747ff7ce222cc5e4fe13081c4d4dc58b319
Pepe Valdivia <jvaldivia@softweb.pe>
2018-08-14 18:40:32 -0500

    CRUD de extensiones

--- archivos/controllers/extension.rb
+++ archivos/controllers/extension.rb
@@ -63,4 +63,68 @@ App::Archivos.controllers :extension do
     status status
     rpta.to_json
   end
+
+  post :guardar, :map => '/extension/guardar' do
+    rpta = nil
+    status = 200
+    data = JSON.parse(params[:data])
+    nuevos = data['nuevos']
+    editados = data['editados']
+    eliminados = data['eliminados']
+    rpta = []
+    array_nuevos = []
+    error = false
+    execption = nil
+    DB_ARCHIVOS.transaction do
+      begin
+        if nuevos.length != 0
+          nuevos.each do |nuevo|
+            n = Models::Archivos::Extension.new(
+                  :nombre => nuevo['nombre'],
+                  :mime => nuevo['mime'],
+                )
+            n.save
+            t = {:temporal => nuevo['id'], :nuevo_id => n.id}
+            array_nuevos.push(t)
+          end
+        end
+        if editados.length != 0
+          editados.each do |editado|
+            e = Models::Archivos::Extension.where(:id => editado['id']).first
+            e.nombre = editado['nombre']
+            e.mime = editado['mime']
+            e.save
+          end
+        end
+        if eliminados.length != 0
+          eliminados.each do |eliminado|
+            Models::Archivos::Extension.where(:id => eliminado).delete
+          end
+        end
+      rescue Exception => e
+        Sequel::Rollback
+        error = true
+        execption = e
+      end
+    end
+    if error == false
+      rpta = {
+        :tipo_mensaje => 'success',
+        :mensaje => [
+          'Se ha registrado los cambios en las extensiones',
+          array_nuevos
+          ]
+        }
+    else
+      status = 500
+      rpta = {
+        :tipo_mensaje => 'error',
+        :mensaje => [
+          'Se ha producido un error en guardar la tabla de extensiones',
+          execption.message]
+        }
+    end
+    status status
+    rpta.to_json
+  end
 end
