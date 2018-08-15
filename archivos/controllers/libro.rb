80e3bf011105c7b2f2356e0ca4acf2572225988b
Pepe Valdivia <jvaldivia@softweb.pe>
2018-08-14 22:22:35 -0500

    Archivos, asociar autor(es) a libro

--- archivos/controllers/libro.rb
+++ archivos/controllers/libro.rb
@@ -162,4 +162,66 @@ App::Archivos.controllers :libro do
     status status
     rpta.to_json
   end
+
+  post :guardar_autor, :map => '/libro/guardar/autor' do
+    rpta = nil
+		status = 200
+		data = JSON.parse(params[:data])
+		nuevos = data['nuevos']
+		editados = data['editados']
+		eliminados = data['eliminados']
+		libro_id = data['extra']['libro_id']
+		rpta = []
+		array_nuevos = []
+		error = false
+		execption = nil
+		DB_ARCHIVOS.transaction do
+			begin
+				if nuevos.length != 0
+					nuevos.each do |nuevo|
+						n = Models::Archivos::LibroAutor.new(:libro_id => libro_id, :autor_id => nuevo['autor_id'])
+						n.save
+						t = {:temporal => nuevo['id'], :nuevo_id => n.id}
+						array_nuevos.push(t)
+					end
+				end
+				if editados.length != 0
+					editados.each do |editado|
+						e = Models::Archivos::LibroAutor.where(:id => editado['id']).first
+						e.autor_id = editado['autor_id']
+						e.libro_id = libro_id
+						e.save
+					end
+				end
+				if eliminados.length != 0
+					eliminados.each do |eliminado|
+						Models::Archivos::LibroAutor.where(:id => eliminado).delete
+					end
+				end
+			rescue Exception => e
+				Sequel::Rollback
+				error = true
+				execption = e
+			end
+		end
+		if error == false
+			rpta = {
+				:tipo_mensaje => 'success',
+				:mensaje => [
+					'Se ha registrado los cambios en las asociaciones de los autores al libro',
+					array_nuevos
+					]
+				}
+		else
+			status = 500
+			rpta = {
+				:tipo_mensaje => 'error',
+				:mensaje => [
+					'Se ha producido un error en asociar los autores al libro',
+					execption.message]
+				}
+		end
+    status status
+    rpta.to_json
+  end
 end
---
+++ archivos/models/libro_autor_model.rb
@@ -0,0 +1,7 @@
+module Models
+  module Archivos
+    class LibroAutor < Sequel::Model(DB_ARCHIVOS[:libros_autores])
+
+    end
+  end
+end
